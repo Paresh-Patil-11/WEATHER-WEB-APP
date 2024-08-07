b5a42f865e6e3cfa7b53813c41464f72a0d1ffc7
Paresh Patil <pareshpatilprof@gmail.com>
2024-08-07 23:40:33 +0530

    Update settings.py

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -112,13 +112,23 @@ USE_I18N = True
 
 USE_TZ = True
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'paresh@12345')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['pareshpatil1.pythonanywhere.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "weather" / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+#add for pythonforevery
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
